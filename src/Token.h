c911c3e7964c438a9246608a9e202402822a1393
EmeraldWolfram <stevenfgy@yahoo.com>
2015-06-29 20:36:28 +0800

    Merged Token.h file from String_Tokenizer

--- src/Token.h
+++ src/Token.h
@@ -1,40 +1,67 @@
 #ifndef Token_H
 #define Token_H
-#define MUL 30
-#define DIV 30
-#define SUB 20
-#define ADD 20
-#define EOT 0
+
+#include <stdint.h>
 
 typedef enum {
-  TOKEN_INTEGER_TYPE,
-  TOKEN_OPERATOR_TYPE,
-}TokenType;
+	TOKEN_UNKNOWN_TYPE,
+	TOKEN_INTEGER_TYPE,
+	TOKEN_OPERATOR_TYPE,
+	TOKEN_FLOAT_TYPE,
+	TOKEN_STRING_TYPE,
+	TOKEN_IDENTIFIER_TYPE,
+} TokenType;
 
 typedef enum {
-  PREFIX,
-  INFIX,
-  POSTFIX,
+	PREFIX, //-2
+	INFIX,	//2*2
+	POSTFIX	//x++
 } Arity;
 
+typedef enum {
+	NONE,
+	LEFT_TO_RIGHT,
+	RIGHT_TO_LEFT,
+} Associativity;
+
 typedef struct {
-  TokenType type;
-}Token;
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+} Token;
 
-typedef struct{
-  TokenType type;
-  int value;
-}IntegerToken;
+typedef struct {
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+	int value;
+} IntegerToken;
 
-typedef struct{
-  TokenType type;
-  char *symbol;
-  Arity arity;
-  int bindingPower;
-  Token *token[0];
+typedef struct {
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+	double value;
+} FloatToken;
+
+typedef struct {
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+	char *name;
+} IdentifierToken, StringToken;
+
+typedef struct {
+	TokenType type;
+  uint32_t startColumn;
+  uint32_t length;
+	char *symbol;
+	Arity arity;
+	Token *token[0];
 } OperatorToken;
 
-OperatorToken *createOperatorToken(char *symbol, Arity arity);
-IntegerToken *createIntegerToken(int value);
+Token *createOperatorToken(char *symbol, Arity AR);
+Token *createIntegerToken(int value);
 
 #endif // Token_H
+
